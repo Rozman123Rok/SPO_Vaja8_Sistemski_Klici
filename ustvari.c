3d7c88d5d8acc8659ecd1c44e3f39962f180752e
Rok Rozman <rok.rozman2@student.um.si>
2020-05-04 15:24:29 +0200

    Narejeno do konca in pripravljeno za oddajo

--- ustvari.c
+++ ustvari.c
@@ -12,30 +12,56 @@
 #include <sys/types.h>
 // Rok Rozman
 
-int main(){
-    char *imenik = "spoimenik"; // nasa mapa ki jo bomo naredili
-    struct stat dat = {0}; // struktura
-    
-    if(stat(imenik, &dat) == -1) // funkcija ki nam vrne informacijo o dat
-    {
-        mkdir(imenik, 0777); // ce je ni jo ustvarimo
+char* dovoljenja(char *datoteka){ // izpise katera dovoljenja ima datoteka
+    struct stat st;
+    char *temp = malloc(sizeof(char) * 9 + 1); // ker rabimo 9 mest za pisanje pravic
+    if(stat(datoteka, &st) == 0){
+        mode_t perm = st.st_mode; // PRIDOBIMO VREDNOST DOVOLJEN
+
+        if(perm & S_IRUSR){temp[0]='r';} // CE IMA USER PRAVICO BRANJA
+        else{temp[0]='-';}
+        if(perm & S_IWUSR){temp[1]='w';} // CE IMA USER PRAVICO PISANJA
+        else{temp[1]='-';}
+        if(perm & S_IXUSR){temp[2]='x';} // CE IMA USER PRAVICO ZAGNAT
+        else{temp[2]='-';}
+        if(perm & S_IRGRP){temp[3]='r';} // CE IMA GROUP PRAVICO BRANJA
+        else{temp[3]='-';}
+        if(perm & S_IWGRP){temp[4]='w';} // CE IMA GROUP PRAVICO PISANJA
+        else{temp[4]='-';}
+        if(perm & S_IXGRP){temp[5]='x';} // CE IMA GROUP PRAVICO ZAGNAT
+        else{temp[5]='-';}
+        if(perm & S_IROTH){temp[6]='r';} // CE IMAJO OSTALI PRAVICO BRANJA
+        else{temp[6]='-';}
+        if(perm & S_IWOTH){temp[7]='w';} // CE IMAJO OSTALI PRAVICO PISANJA
+        else{temp[7]='-';}
+        if(perm & S_IXOTH){temp[8]='x';} // CE IMAJO OSTALI PRAVICO ZAGANJANJA
+        else{temp[8]='-';}
+        temp[9]='\0'; // ZADNJI ZNAK
+        return temp; // VRNEMO REZULTAT   
     }
+    else{
+        return -1; // ce je error
+    }   
+}
 
-    char* pot = malloc(strlen(imenik) + strlen("spozbirka.txt") + 2); // si nastavimo prostor ki ga rabimo
-    pot = strcpy(pot, imenik); // kopiramo imenik v pot
-    pot = strcat(pot, "/spozbirka.txt \n"); // mu dodamo se datoteko kjer je
+int main(){
+    long ustvari_mapo = syscall(SYS_mkdir, "spoimenik", 0777); // ustvarim mapo 
+    if(ustvari_mapo < 0){printf("Error pri ustvarjanju mape\n"); return -1;} // preverimo ce nam vrze error
 
-    printf("Rok Rozman\n");
-    printf("pot: %s", pot);
-
-    long temp;
-    temp = syscall(SYS_mkdir, "test", 0777); // ustvarim mapo 
-    //char* temp1 = 
-    long sprememba = syscall(SYS_chdir, "spoimenik"); // spremenimo dir
-    //long ntemp = syscall(SYS_mkdir, "test1", 0777); // ustvarim mapo 
+    long sp_dir = syscall(SYS_chdir, "spoimenik"); // spremenimo dir v mapo ki smo jo ustvarili
+    if(sp_dir < 0){printf("Error pri spreminjanju dir\n"); return -1;}
     
-    long ustvari_file = syscall(SYS_creat, "spozbirka.txt"); // ustvarimo datoteko
+    long ustvari_file = syscall(SYS_creat, "spozbirka.txt"); // ustvarimo datoteko brez podanih pravic
+    if(ustvari_file < 0){printf("Error pri ustvarjanju dat\n"); return -1;}
 
+    printf("Pravice pred (spozbirka.txt): \n");
+    printf("%s\n" , dovoljenja("spozbirka.txt"));    
+
+    long pravice = syscall(SYS_chmod,"spozbirka.txt", 00640); // dodamo pravice 6- user read and write, 4 - group read
+    if(pravice < 0){printf("Error pri postavljanju pravic\n"); return -1;}
+
+    printf("Pravice po (spozbirka,txt): \n");
+    printf("%s\n" , dovoljenja("spozbirka.txt"));
 
     return 0;
 }
